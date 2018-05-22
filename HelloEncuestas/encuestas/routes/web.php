678e0db798e87f3e9f72257e5ef39861eff49c34
Hello Team Omar <helloteam.omar@gmail.com>
2018-05-22 17:40:53 -0500

    rutas
    creacion de rutas para aceder a los nuevos modulos

--- HelloEncuestas/encuestas/routes/web.php
+++ HelloEncuestas/encuestas/routes/web.php
@@ -18,3 +18,85 @@ Route::post('guardarContestada', 'EncuestaEspecificaContestadaController@guardar
 Route::resource('encuestaEspecifica', 'EncuestaEspecificaController');
 Route::resource('encuestaEspecificaContestada', 'EncuestaEspecificaContestadaController');
 
+
+Route::get('/Home', [
+    'uses' =>'HomeController@index',
+    'as' => 'Home.index'
+
+]);
+
+
+Route::get('Usuarios/crear', [
+    'uses' =>'UsuariosController@create',
+    'as' => 'Usuarios.crear'
+
+]);
+
+
+Route::post('Usuarios/store', [
+    'uses' =>'UsuariosController@store',
+    'as' => 'Usuarios.store'
+
+]);
+
+
+Route::get('Usuarios/login', [
+    'uses' =>'UsuariosController@login',
+    'as' => 'Usuarios.login'
+
+]);
+
+Route::get('Usuarios/logout', [
+    'uses' =>'UsuariosController@logout',
+    'as' => 'Usuarios.logout'
+
+]);
+
+Route::get('Usuarios/mostrar', [
+    'uses' =>'UsuariosController@show',
+    'as' => 'Usuarios.mostrar'
+
+]);
+
+Auth::routes();
+
+Route::post('Usuarios/login', [
+    'uses' =>'UsuariosController@loginuser',
+    'as' => 'Usuarios.login'
+
+]);
+
+
+
+Route::get('Usuarios/edit', [
+    'uses' =>'UsuariosController@edit',
+    'as' => 'Usuarios.edit'
+
+]);
+
+
+Route::put('Usuarios/update', [
+    'uses' =>'UsuariosController@update',
+    'as' => 'Usuarios.update'
+
+]);
+
+
+Route::get('Usuarios/Administrador_Correos', [
+    'uses' =>'CorreoController@administrador',
+    'as' => 'Usuarios.Administrador_Correos'
+
+]);
+
+
+Route::get('Usuarios/Lista_Correos', [
+    'uses' =>'CorreoController@lista',
+    'as' => 'Usuarios.lista_Correos'
+
+]);
+
+
+
+Route::post('Usuarios/Enviarcorreos','CorreoController@correos');
+
+Route::post('Usuarios/Listacorreos','CorreoController@listasend');
