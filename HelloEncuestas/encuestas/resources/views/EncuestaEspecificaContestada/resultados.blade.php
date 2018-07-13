19a43d077d98be9e965a8d6e01dfacc4c3c5a891
Daniel_Rojas <helloteam.daniell@gmail.com>
2018-07-12 19:21:30 -0500

    update
    pendiente edit.blade
    controllers encuenstas

--- HelloEncuestas/encuestas/resources/views/EncuestaEspecificaContestada/resultados.blade.php
+++ HelloEncuestas/encuestas/resources/views/EncuestaEspecificaContestada/resultados.blade.php
@@ -1,15 +1,36 @@
 @extends('layouts.menu')
 @section('content')
 
-<<<<<<< HEAD
-=======
------------------------------------------------------>
->>>>>>> dc6323e77c90b77cd7eb6db6b8c1b3caa58a12e2
-<head><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
 <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
 <script src="http://code.jquery.com/ui/1.11.0/jquery-ui.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.4.1/jspdf.min.js"></script>
+<script lang="javascript" src="dist/xlsx.full.min.js"></script>
+<script type="text/javascript" src="shim.min.js"></script>
+<!-- after the shim is referenced, add the library -->
+<script type="text/javascript" src="xlsx.full.min.js"></script>
 
+<script src="https://www.amcharts.com/lib/3/amcharts.js"></script>
+<script src="https://www.amcharts.com/lib/3/serial.js"></script>
+<script src="https://www.amcharts.com/lib/3/themes/light.js"></script>
+<script src="https://www.amcharts.com/lib/3/plugins/export/export.js"></script>
+
+
+<style type="text/css">
+
+#downloadBtn {
+	
+  cursor:pointer;
+}
+
+.chartdiv {
+    width       : 800px;
+    height      : 220px;
+}
+</style>
 </head>
 	<div class="container-fluid">
 		<div class="row">
@@ -29,27 +50,29 @@
 							</div>
 						</div>
 						@endif
-						@endforeach
-						<div class="row">
-							<div class="col-xs-5">
-								<label>Pregunta: Encuesta Básica</label>
-								<canvas id="grafica" width="200" height="200"></canvas>
-							</div>
-						</div>
-					</div>
+						@endforeach	
 					<div class="col-xs-12 col-md-1 col-lg-1">
-						
+								
+	
+   </div>
+   </div>
+   <div class="container" id="chart">
+   <div style="center">
+     <canvas id="Graf01" width="100" height="50"></canvas><br>
+     <button id="downloadBtn" class="btn btn-info">Descargar PDF</button>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 </body>
-	<script>
+<script>
 		var cadena = "";
 		var datos = "";
 		var respuestas = "";
 		var datosSeparados = "";
+		var downloadBtn = document.getElementById('downloadBtn');
+		var canvas = document.getElementById("Graf01");
 		var frecuencias = [];
 		$(document).ready(function(){
 			@foreach($preguntas as $p)
@@ -79,7 +102,7 @@
 				@endforeach
 				var grafica = $('#graf{{$p->id_pesp}}');
 
-				var grafica = new Chart(grafica, {
+				var grafica = new Chart(canvas, {
 					type: 'doughnut',
 					data: {
 				        labels: respuestas{{$p->id_pesp}},
@@ -110,6 +133,18 @@
 			}
 			@endif
 			@endforeach
-		});
+	
+    downloadBtn.addEventListener("click", function() {
+      var d = new Date();
+      var n = d.toISOString();
+      // only jpeg is supported by jsPDF
+      var imgData = canvas.toDataURL("image/png", 1.0);
+      var pdf = new jsPDF();
+      pdf.setFontSize(20);
+	  pdf.text(10,10,'{{$p->pregunta}}');
+      pdf.addImage(imgData, "JPEG", 2, 20,100,40);
+      pdf.save(n+"-Encuesta.pdf");
+    }, false);
+  });
 	</script>
 @endsection
